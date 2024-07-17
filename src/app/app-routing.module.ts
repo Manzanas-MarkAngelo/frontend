c688a156af9aa1c4599245f068dabe01c7c8e91f
Odrunia_Mark_Ryan <odruniamarkryan@gmail.com>
2024-07-17 12:14:08 +0800

    add routings

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegisterComponent } from './client/register/register.component';
-
 import { AdministratorComponent} from './client/administrator/administrator.component';
+import { TimeInComponent } from './client/time-in/time-in.component';
+import { TimeOutComponent } from './client/time-out/time-out.component';
+import { SearchBookComponent } from './client/search-book/search-book.component';
+import { ServicesComponent } from './client/services/services.component';
+import { ClientComponent } from './client/client.component';
+
 const routes: Routes = [
-  {path: 'administrator', component: AdministratorComponent}
+  { path: '', redirectTo: '/client', pathMatch: 'full' },
+  { path: 'administrator', component: AdministratorComponent },
+  { path: 'time-in', component: TimeInComponent },
+  { path: 'time-out', component: TimeOutComponent },
+  { path: 'search', component: SearchBookComponent },
+  { path: 'services', component: ServicesComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'client', component: ClientComponent },
 ];
 
 @NgModule({
