4cc123f2ae3588d11f00365df10d07460d536092
Odrunia_Mark_Ryan <odruniamarkryan@gmail.com>
2024-07-19 16:11:01 +0800

    create add success component

---
+++ src/app/admin/add-success/add-success.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-success',
+  templateUrl: './add-success.component.html',
+  styleUrl: './add-success.component.css'
+})
+export class AddSuccessComponent {
+
+}
