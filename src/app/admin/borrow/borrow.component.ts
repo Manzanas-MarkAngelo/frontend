e81077ac7c2252f4b361311d90b080e03edeb88f
Odrunia_Mark_Ryan <odruniamarkryan@gmail.com>
2024-07-19 16:21:27 +0800

    fix category placeholder

--- src/app/admin/borrow/borrow.component.ts
+++ src/app/admin/borrow/borrow.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrl: './borrow.component.css'
 })
 export class BorrowComponent {
-  categoryPlaceholder: string = 'Category'
+  categoryPlaceholder: string = 'Choose category'
 
   CategoryPlaceholder(value: string) {
     this.categoryPlaceholder = value;
