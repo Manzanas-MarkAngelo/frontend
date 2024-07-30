06d759d95a3377abd56cc7e62ecb4af285447025
ChrystineNoelle <asebuquechrystine@gmail.com>
2024-07-31 02:08:23 +0800

    implement administrator login

---
+++ src/services/admin-login.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from './environments/local-environment';
+
+@Injectable()
+export class AdminLoginService {
+  private apiUrl = `${environment.apiUrl}/admin_login.php`;
+
+  constructor(private http: HttpClient) { }
+
+  login(username: string, password: string): Observable<any> {
+    return this.http.post<any>(this.apiUrl, { username, password });
+  }
+}
