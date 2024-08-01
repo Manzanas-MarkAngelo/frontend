9b67d849f10d71db7a8a7f83565e0167dc092f1f
ManzanasMark <markangelommanzanas@iskolarngbayan.pup.edu.ph>
2024-08-01 20:27:37 +0800

    add api backend for adding book

---
+++ src/services/add-material.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from './environments/local-environment';
+
+@Injectable()
+export class AddMaterialService {
+
+  private apiUrl = `${environment.apiUrl}/add_book.php`;
+
+  constructor(private http: HttpClient) { }
+
+  addBook(bookDetails: any): Observable<any> {
+    return this.http.post<any>(this.apiUrl, bookDetails);
+  }
+}
