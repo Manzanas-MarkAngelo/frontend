4af40d4701e9d578bb589ae9834d8905c838962b
ManzanasMark <markangelommanzanas@iskolarngbayan.pup.edu.ph>
2024-08-20 22:48:28 +0800

    add pup logo to borrowers report generation for PDF

--- src/services/pdf-report-borrowers.service.ts
+++ src/services/pdf-report-borrowers.service.ts
@@ -13,13 +13,36 @@ export class PdfReportBorrowersService {
     setLoading: (loading: boolean) => void, setShowInitialDisplay: (show: boolean) => void) {
     setLoading(true);
     setShowInitialDisplay(false);
-
+  
     const doc = new jsPDF('landscape');
     const title = "Polytechnic University of the Philippines - Taguig Campus";
     const subtitle = "PUPT BORROWERS REPORT";
-    const dateAndTime = `YEAR AS OF ${this.currentDateYearService
-          .getCurrentYearAndDate('no_date')}`;
-
+    const dateAndTime = `YEAR AS OF ${this.currentDateYearService.getCurrentYearAndDate('no_date')}`;
+  
+    const imgBase64 = '../assets/pup.png';
+    const imgXPosition = 50;
+    const imgYPosition = 2;
+    const imgWidth = 20;
+    const imgHeight = 20;
+  
+    doc.addImage(imgBase64, 'PNG', imgXPosition, imgYPosition, imgWidth, imgHeight);
+  
+    const textStartY = 10;
+  
+    doc.setFontSize(16);
+    doc.setTextColor("#800000");
+    doc.text(title, doc.internal.pageSize.getWidth() / 2, textStartY, { align: "center" });
+  
+    doc.setFontSize(12);
+    doc.setTextColor("#000000");
+    doc.text(subtitle, doc.internal.pageSize.getWidth() / 2, textStartY + 8, { align: "center" });
+  
+    doc.setFontSize(12);
+    doc.setTextColor("#252525");
+    doc.text(dateAndTime, doc.internal.pageSize.getWidth() / 2, textStartY + 15, { align: "center" });
+  
+    let startY = textStartY + 20;
+  
     this.returnService.getBorrowingData(dateFrom, dateTo).subscribe(
       response => {
         const borrowersData = response || [];
@@ -33,23 +56,9 @@ export class PdfReportBorrowersService {
           borrower.due_date,
           borrower.remark
         ]);
-
-        doc.setFontSize(16);
-        doc.setTextColor("#800000");
-        doc.text(title, doc.internal.pageSize
-            .getWidth() / 2, 15, { align: "center" });
-
-        doc.setFontSize(12);
-        doc.setTextColor("#000000");
-        doc.text(subtitle, doc.internal.pageSize
-            .getWidth() / 2, 23, { align: "center" });
-
-        doc.setFontSize(12);
-        doc.setTextColor("#252525");
-        doc.text(dateAndTime, doc.internal.pageSize
-            .getWidth() / 2, 30, { align: "center" });
-
-        let startY = 35;
+  
+        const maxTableY = doc.internal.pageSize.getHeight() - 30; // Set the max Y position for the table
+  
         autoTable(doc, {
           head: [[ 'Title', 'Author', 'User Type', 'Name', 'Course', 'Claim Date', 'Due Date', 'Remark']],
           body: tableData,
@@ -60,53 +69,46 @@ export class PdfReportBorrowersService {
             0: { cellWidth: 45 },
             1: { cellWidth: 40 },
             2: { cellWidth: 20 },
-            3: { cellWidth: 45 },
+            3: { cellWidth: 50 },
             4: { cellWidth: 20 },
             5: { cellWidth: 30 },
             6: { cellWidth: 30 },
-            7: { cellWidth: 30 },
+            7: { cellWidth: 25 },
           },
           styles: {
             overflow: 'linebreak',
             cellPadding: 2
           },
           didDrawPage: (data) => {
-            startY = data.cursor.y;
+            if (data.cursor.y >= maxTableY) {
+              doc.addPage();
+              doc.setFontSize(10);
+              const labelXPosition = 20;
+              const valueXPosition = 80;
+              const footerY = doc.internal.pageSize.getHeight() - 20;
+  
+              doc.text(`Report Generated On:`, labelXPosition, footerY);
+              doc.text(`${this.currentDateYearService.getCurrentYearAndDate('get_date')}`, valueXPosition, footerY);
+  
+              doc.text(`Total Borrowers Records:`, labelXPosition, footerY + 5);
+              doc.text(`${borrowersData.length}`, valueXPosition, footerY + 5);
+  
+              if (dateFrom && dateTo) {
+                doc.text(`Borrowing Data From:`, labelXPosition, footerY + 10);
+                doc.text(`${this.currentDateYearService.formatDateString(dateFrom)} - ${this.currentDateYearService.formatDateString(dateTo)}`, valueXPosition, footerY + 10);
+              }
+  
+              // Reset the startY position for the next page
+              data.cursor.y = 10;
+            }
           }
         });
-
-        const labelXPosition = 20;
-        const valueXPosition = 80;
-
-        doc.setFontSize(10);
-        // Date generated
-        doc.text(`Report Generated On:`, labelXPosition, 
-            doc.internal.pageSize.getHeight() - 20);
-        doc.text(`${this.currentDateYearService
-              .getCurrentYearAndDate('get_date')}`, valueXPosition, 
-            doc.internal.pageSize.getHeight() - 20);
-
-        // Total records
-        doc.text(`Total Borrowers Records:`, labelXPosition, 
-            doc.internal.pageSize.getHeight() - 15);
-        doc.text(`${borrowersData.length}`, valueXPosition, 
-            doc.internal.pageSize.getHeight() - 15);
-
-        // Filter
-        if (dateFrom && dateTo) {
-          doc.text(`Borrowing Data From:`, labelXPosition, 
-              doc.internal.pageSize.getHeight() - 10);
-          doc.text(`${this.currentDateYearService
-              .formatDateString(dateFrom)} - ${this.currentDateYearService
-              .formatDateString(dateTo)}`, valueXPosition, 
-              doc.internal.pageSize.getHeight() - 10);
-        }
-
+  
         const pdfBlob = doc.output('blob');
         const pdfUrl = URL.createObjectURL(pdfBlob);
         const iframe = document.getElementById(iframeId) as HTMLIFrameElement;
         iframe.src = pdfUrl;
-
+  
         setLoading(false);
       },
       error => {
@@ -115,4 +117,7 @@ export class PdfReportBorrowersService {
       }
     );
   }
+  
+              
+              
 }
