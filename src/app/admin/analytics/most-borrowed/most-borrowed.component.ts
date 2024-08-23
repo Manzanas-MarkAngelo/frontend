6e5bb2b1e556e2a7218ca36360c7760cb47a0c86
Odrunia_Mark_Ryan <odruniamarkryan@gmail.com>
2024-08-23 11:53:03 +0800

    create child component of analytics component, most borrowed books and top library users

---
+++ src/app/admin/analytics/most-borrowed/most-borrowed.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-most-borrowed',
+  templateUrl: './most-borrowed.component.html',
+  styleUrl: './most-borrowed.component.css'
+})
+export class MostBorrowedComponent {
+
+}
