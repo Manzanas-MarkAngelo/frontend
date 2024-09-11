10a98e919b78a75086889bf6e54559610d84c1aa
Odrunia_Mark_Ryan <odruniamarkryan@gmail.com>
2024-09-11 16:38:08 +0800

    make admin side mobile responsive

--- src/app/admin/borrow/borrow.component.ts
+++ src/app/admin/borrow/borrow.component.ts
@@ -13,7 +13,7 @@ export class BorrowComponent implements OnInit {
   totalItems: number = 0;
   totalPages: number = 0;
   currentPage: number = 1;
-  itemsPerPage: number = 13;
+  itemsPerPage: number = 12;
   searchTerm: string = '';
   category: string = '';
   private searchTerms = new Subject<string>();
